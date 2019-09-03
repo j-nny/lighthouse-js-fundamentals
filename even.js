57639ebb546d22c619db3320aed53f49629417ee
Jenny Nguy <j.nguy809@gmail.com>
2019-09-03 20:09:02 +0000

    use function to determine if a number is even

---
+++ even.js
@@ -0,0 +1,6 @@
+const isEven = function (num) {
+  return num % 2 === 0; // returns the Boolean value if num is even or not
+}
+
+console.log(isEven(10)); //console.log is a function that is pre-defined by JS so it does not have to be defined in this code
+console.log(isEven(11));
